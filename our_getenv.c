a3f2f1e47ebe4639cf2de6d4eb4d4fd84ed438af
Paulette Wacheke <paulettekinguri@gmail.com>
2023-09-20 10:08:43 +0000

    simple shell

--- our_getenv.c
+++ our_getenv.c
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 #include "main.h"
-=======
-#include "shell.h"
 
->>>>>>> 8f649e5da1d57fa4aa9f618392cfb95843a61479
 /**
  * our_getenv - function gets the environment of a variabe
  * @var: variable name
